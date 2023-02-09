ea5b5bfc45dea7601a790de096aeab7e0fab7225
JonasR30011 <jonasrocha210@gmail.com>
2023-02-09 00:48:20 -0300

    Resolução do exercício2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,5 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      "
+const removeSpace = minhaString.trim()
+const remove = minhaString.replace("________","sticioso")
+console.log(minhaString.length)
+console.log(removeSpace.length)
